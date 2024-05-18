fdf0f8426cd341173cade872aed277c008db111f
rebecacoelho <rebecasantoscoelho@gmail.com>
2024-05-18 17:38:40 -0300

    ajust class medicine

--- src/main/java/com/example/bulajapostgree/medicines/Medicine.java
+++ src/main/java/com/example/bulajapostgree/medicines/Medicine.java
@@ -1,9 +1,6 @@
 package com.example.bulajapostgree.medicines;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
@@ -14,12 +11,28 @@ public class Medicine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 100)
     private String nome;
+
+    @Column(length = 100)
     private String nomeGenerico;
+
+    @Column(columnDefinition = "TEXT")
     private String formaDeDosagem;
+
+    @ElementCollection
     private List<String> remediosRelacionados;
+
+    @Column(columnDefinition = "TEXT")
     private String descricao;
+
+    @Column(columnDefinition = "TEXT")
     private String efeitosColaterais;
+
+    @Column(columnDefinition = "TEXT")
     private String tratamento;
+
+    @Column(columnDefinition = "TEXT")
     private String precaucoes;
 }
